6ff7557e5dabbbb87f78e79d3f1cbb7626775356
Nasser BOUKEHIL <boukehil.nasreddine@gmail.com>
2021-01-14 23:15:51 +0100

    added test dependencies, env and config

---
+++ src/test/java/com/nasserBoukehil/worldGdp/test/config/TestDBConfiguration.java
@@ -0,0 +1,32 @@
+package com.nasserBoukehil.worldGdp.test.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
+@Configuration
+public class TestDBConfiguration {
+	
+	@Bean
+	public DataSource dataSource() {
+		return new EmbeddedDatabaseBuilder()
+				.generateUniqueName(true)
+				.setType(EmbeddedDatabaseType.H2)
+				.setScriptEncoding("UTF-8")
+				.ignoreFailedDrops(true)
+				.addScript("h2_world.sql")
+				.build();
+	}
+	
+	@Bean("testTemplate")
+	public NamedParameterJdbcTemplate namedParamJdbcTemplate() {
+		NamedParameterJdbcTemplate namedParameterJdbcTemplate = new NamedParameterJdbcTemplate(dataSource());
+		return namedParameterJdbcTemplate;
+	}
+	
+
+}
